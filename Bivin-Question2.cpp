a45d8e74a3ca9bcda61597dd48617b6274d0cd53
Bivin Varkey Varghese <bivinvvpacoeg@gmail.com>
2020-04-06 21:20:58 +0530

    added second cpp question

---
+++ Bivin-Question2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num,d=0,n,r,p=0,a=1;
+char choice;
+cin<<num;
+cin<<choice;
+swithc(choice)
+{
+case a 
+n=num;
+while(n==0)
+{
+n=n/10;
+d++;
+}
+n=num;
+while(n!=0)
+{
+r=n%10;
+a==a*sqr(r,d);
+n=n/10;
+}
+cout<<a<<"\n";  
+case p 
+n=num;
+while(n!=0)
+{
+r=n%10;
+p==p+10+r;
+n=n/10;
+}
+cout<<p<<"\n";
+continue;
+}
+}
